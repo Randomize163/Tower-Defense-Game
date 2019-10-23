cf4faccfd3b3c87851a4472ef5c60514c6d27082
Alexander Gurevich <guran1996@gmail.com>
2019-10-23 20:07:20 +0300

    [Maze] Code refactoring / Cleanup
    Signed-off-by: Alexander Gurevich <guran1996@gmail.com>

--- scripts/td-map.js
+++ scripts/td-map.js
@@ -84,26 +84,40 @@ class Stack
     }
 }
 
-class CMazeGenerator 
+class CMaze
 {
-    static generateMaze(width, height)
+    constructor(width, height)
     {
-        let maze = Array.from(Array(width), () => new Array(height));
-        for (let i = 0; i < maze.length; i++)
+        this.width = width;
+        this.height = height;
+        this.mazePixels = [];
+        this.mazeTiles = [];
+        
+        this.initializeMazeTiles();
+        this.generateMazeTiles();
+        this.transformMazeToPixels();
+    }
+
+    initializeMazeTiles()
+    {
+        this.mazeTiles = Array.from(Array(this.width), () => new Array(this.height));
+        for (let i = 0; i < this.mazeTiles.length; i++)
         {
-            for (let j = 0; j < maze[0].length; j++)
+            for (let j = 0; j < this.mazeTiles[0].length; j++)
             {
-                maze[i][j] = {
+                this.mazeTiles[i][j] = {
                     "tileType": TilesType.empty,
-                    "visited": false,
                     "walls": WallsType.all
                 };
             }
         }
+    }
 
+    generateMazeTiles()
+    {
+        let visited = Array.from(Array(this.width), () => new Array(this.height).fill(false));
         let stack = new Stack();
-        let visited = 0;
-        let startPosition = [Math.floor(Math.random()*width), Math.floor(Math.random()*height)];
+        let startPosition = [Math.floor(Math.random() * this.width), Math.floor(Math.random() * this.height)];
         
         stack.push(startPosition);
 
@@ -115,47 +129,10 @@ class CMazeGenerator
             }
 
             let [currentX, currentY] = stack.top();
-            maze[currentX][currentY].visited = true;
+            visited[currentX][currentY] = true;
             
             // find potential nexts
-            let nexts = [];
-    
-            if (currentX + 1 < maze.length && maze[currentX + 1][currentY].visited == false)
-            {
-                nexts.push({
-                    "position": [currentX + 1, currentY],
-                    "direction": 'right',
-                    "breakWallForNext": 'left'
-                });
-            }
-            
-            if (currentX - 1 >= 0 && maze[currentX - 1][currentY].visited == false)
-            {
-                nexts.push({
-                    "position": [currentX - 1, currentY],
-                    "direction": 'left',
-                    "breakWallForNext": 'right'
-                });
-            }
-            
-            if (currentY + 1 < maze[0].length && maze[currentX][currentY + 1].visited == false)
-            {
-                nexts.push({
-                    "position": [currentX, currentY + 1],
-                    "direction": 'up',
-                    "breakWallForNext": 'down'
-                });
-            }
-            
-            if (currentY - 1 >= 0 && maze[currentX][currentY - 1].visited == false)
-            {
-                nexts.push({
-                    "position": [currentX, currentY - 1],
-                    "direction": 'down',
-                    "breakWallForNext": 'up'
-                });
-            }
-            
+            let nexts = this.getPotentialNextTilesFor(currentX, currentY, visited);
             if (nexts.length == 0)
             {
                 stack.pop();
@@ -163,126 +140,149 @@ class CMazeGenerator
             }
 
             // choose next
-            let next = nexts[Math.floor(Math.random() * nexts.length)];
+            let selectedNext = nexts[Math.floor(Math.random() * nexts.length)];
             
             // break the wall
-            let [nextX, nextY] = next.position;
-            maze[nextX][nextY].walls       &= ~WallsType[next.breakWallForNext];
-            maze[currentX][currentY].walls &= ~WallsType[next.direction];
+            let [nextX, nextY] = selectedNext.position;
+            this.mazeTiles[nextX][nextY].walls       &= ~WallsType[selectedNext.breakWallForNext];
+            this.mazeTiles[currentX][currentY].walls &= ~WallsType[selectedNext.direction];
 
             // go to next
-            stack.push(next.position);
+            stack.push(selectedNext.position);
         }
-
-        return maze;
     }
 
-    static transformMazeToPixels(maze, addInternalWall = false)
+    getPotentialNextTilesFor(currentX, currentY, visited)
     {
-        let result = Array.from({length: maze.length * 4 + 1}, e => Array(maze[0].length * 4 + 1).fill(TilesType.path));         
-
-        for (let i = 0; i < maze.length; i++)
+        let nexts = [];
+        if (currentX + 1 < this.mazeTiles.length && visited[currentX + 1][currentY] == false)
         {
-            for (let j = 0; j < maze[0].length; j++)
+            nexts.push({
+                "position": [currentX + 1, currentY],
+                "direction": 'right',
+                "breakWallForNext": 'left'
+            });
+        }
+        
+        if (currentX - 1 >= 0 && visited[currentX - 1][currentY] == false)
+        {
+            nexts.push({
+                "position": [currentX - 1, currentY],
+                "direction": 'left',
+                "breakWallForNext": 'right'
+            });
+        }
+        
+        if (currentY + 1 < this.mazeTiles[0].length && visited[currentX][currentY + 1] == false)
+        {
+            nexts.push({
+                "position": [currentX, currentY + 1],
+                "direction": 'up',
+                "breakWallForNext": 'down'
+            });
+        }
+        
+        if (currentY - 1 >= 0 && visited[currentX][currentY - 1] == false)
+        {
+            nexts.push({
+                "position": [currentX, currentY - 1],
+                "direction": 'down',
+                "breakWallForNext": 'up'
+            });
+        }
+
+        return nexts;
+    }
+
+    transformMazeToPixels(addInternalWall = true)
+    {
+        this.mazePixels = Array.from({length: this.mazeTiles.length * 4 + 1}, e => Array(this.mazeTiles[0].length * 4 + 1).fill(TilesType.path));         
+
+        for (let i = 0; i < this.mazeTiles.length; i++)
+        {
+            for (let j = 0; j < this.mazeTiles[0].length; j++)
             {
-                this.transformTileToPixels(result, i, j, maze[i][j], addInternalWall);
+                this.transformTileToPixels(i, j, addInternalWall);
             }
         }
-
-        return result;
     }
 
-    static transformTileToPixels(result, i, j, tile, addInternalWall = false)
+    transformTileToPixels(i, j, addInternalWall = false)
     {
         let [pixelTileCenterX, pixelTileCenterY] = [2 + 4*i, 2 + 4*j];
 
+        let tile = this.mazeTiles[i][j];
+
         if (addInternalWall)
         {
-            result[pixelTileCenterX][pixelTileCenterY] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY] = TilesType.empty;
         }
-
 
         if ((tile.walls & WallsType.left) != 0)
         {
-            result[pixelTileCenterX - 2][pixelTileCenterY]     = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY - 1] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY - 2] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY + 1] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY]     = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY - 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY + 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY + 2] = TilesType.empty;
         }
         else if (addInternalWall)
         {
-            result[pixelTileCenterX - 1][pixelTileCenterY] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 1][pixelTileCenterY] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY] = TilesType.empty;
         }
 
         if ((tile.walls & WallsType.right) != 0)
         {
-            result[pixelTileCenterX + 2][pixelTileCenterY]     = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY - 1] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY + 1] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY - 2] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY]     = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY - 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY + 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY + 2] = TilesType.empty;
         }
         else if (addInternalWall)
         {
-            result[pixelTileCenterX + 1][pixelTileCenterY] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 1][pixelTileCenterY] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY] = TilesType.empty;
         }
 
         if ((tile.walls & WallsType.up) != 0)
         {
-            result[pixelTileCenterX][pixelTileCenterY + 2]     = TilesType.empty;
-            result[pixelTileCenterX + 1][pixelTileCenterY + 2] = TilesType.empty;
-            result[pixelTileCenterX - 1][pixelTileCenterY + 2] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY + 2] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY + 2]     = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 1][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 1][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY + 2] = TilesType.empty;
         }
         else if (addInternalWall)
         {
-            result[pixelTileCenterX][pixelTileCenterY + 1] = TilesType.empty;
-            result[pixelTileCenterX][pixelTileCenterY + 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY + 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY + 2] = TilesType.empty;
         }
 
         if ((tile.walls & WallsType.down) != 0)
         {
-            result[pixelTileCenterX][pixelTileCenterY - 2]     = TilesType.empty;
-            result[pixelTileCenterX + 1][pixelTileCenterY - 2] = TilesType.empty;
-            result[pixelTileCenterX - 1][pixelTileCenterY - 2] = TilesType.empty;
-            result[pixelTileCenterX + 2][pixelTileCenterY - 2] = TilesType.empty;
-            result[pixelTileCenterX - 2][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY - 2]     = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 1][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 1][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX + 2][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX - 2][pixelTileCenterY - 2] = TilesType.empty;
         }
         else if (addInternalWall)
         {
-            result[pixelTileCenterX][pixelTileCenterY - 1] = TilesType.empty;
-            result[pixelTileCenterX][pixelTileCenterY - 2] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY - 1] = TilesType.empty;
+            this.mazePixels[pixelTileCenterX][pixelTileCenterY - 2] = TilesType.empty;
         }
-
-        return result;
     }
 
-    static generatePixelMaze(width, height, addInternalWall = false)
-    {
-        return this.transformMazeToPixels(this.generateMaze(width, height), addInternalWall)
-    }
-
-    static printPixelMaze(pixelMaze)
-    {
-        console.log("Print Maze (width: " + pixelMaze.length + ", length: " + pixelMaze[0].length + "):");
-        
-        const maze = this.pixelMazeToString(pixelMaze);
-
-        console.log(maze);
-    }
-
-    static pixelMazeToString(pixelMaze)
+    mazePixelsToString()
     {
         let maze = "";
-        for (let i = 0; i < pixelMaze.length; i++) 
+        for (let i = 0; i < this.mazePixels.length; i++) 
         {
-            for (let j = 0; j < pixelMaze[0].length; j++)
+            for (let j = 0; j < this.mazePixels[0].length; j++)
             {
-                switch (pixelMaze[i][j])
+                switch (this.mazePixels[i][j])
                 {
                     case TilesType.empty:
                         maze += "▇";
@@ -291,7 +291,7 @@ class CMazeGenerator
                         maze += "　";
                         break;
                     default:
-                        console.error("Unprintable tile type: " + pixelMaze[i][j]);
+                        console.error("Unprintable tile type: " + this.mazePixels[i][j]);
                         debugger;
                 }
             }
@@ -302,31 +302,45 @@ class CMazeGenerator
         return maze;
     }
 
-    static printPixelMazeToHtml(pixelMaze)
+    consoleLogmazePixels()
     {
-        const maze = this.pixelMazeToString(pixelMaze);
-        let out = document.getElementById("output");
-
-		out.innerHTML = maze;
+        console.log("Print Maze (width: " + this.mazePixels.length + ", length: " + this.mazePixels[0].length + "):");
+        console.log(this.mazePixelsToString());
     }
 
-    static test()
+    printmazePixelsToHtml(elementId)
     {
-        console.log(CMazeGenerator.generateMaze(5,5));
-        console.log(CMazeGenerator.generateMaze(50,50));
-        console.log(CMazeGenerator.generateMaze(500,500));
-        //console.log(mazeGenerator.generateMaze(5000,5000));
-        this.printPixelMaze(CMazeGenerator.generatePixelMaze(10, 10, true));  
-        this.printPixelMaze(CMazeGenerator.generatePixelMaze(3, 3, true));  
-        //this.printPixelMaze(mazeGenerator.generatePixelMaze(100, 100, true));  
-        //this.printPixelMazeToHtml(mazeGenerator.generatePixelMaze(50, 50, true));
-
+        let outputElement = document.getElementById(elementId);
+        outputElement.innerHTML = this.mazePixelsToString();
     }
-}
+
+    static simpleTest()
+    {
+        new CMaze(3,3).consoleLogmazePixels();
+        new CMaze(5,10).consoleLogmazePixels();
+    }
+
+    static longTest()
+    {
+        for (let i = 0; i < 10000; i++)
+        {
+            const width = 2 + Math.floor(Math.random() * 500);
+            const height = 2 + Math.floor(Math.random() * 500);
+            new CMaze(width, height);
+
+            if (i % 100 == 0)
+            {
+                console.log("Finished iteration " + i);
+            }
+        }
+        console.log("Finished long test successfully!");
+    }
+} 
 
 export class CMapTest {
     static run()
     {
-        CMazeGenerator.test();
+        CMaze.simpleTest();
+        CMaze.longTest();
     }
 }
